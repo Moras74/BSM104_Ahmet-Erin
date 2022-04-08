a2d5b17b038e086cf976512d8a76d56ecb6a3350
Ahmet Erin <ahmetunivers@gmail.com>
2022-04-08 17:56:46 +0300

    Uygulama

---
+++ Week 7/queue.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+public class queue {
+	public static void main(String[] args) {
+		
+	String data[] = {"java","php","python","C","c++"};
+	
+	Queue<String> q=new LinkedList<String>();
+	for(String word:data) {
+		q.add(word);
+	}
+	System.out.println("queue = "+q);
+	System.out.println(q.size());
+	System.out.println("peek = "+q.peek());	
+	}		
+	}
+
