828baa865ad9dd113b0fecb20e64863d0fb0b57e
Ahmet Erin <ahmetunivers@gmail.com>
2022-04-08 17:51:05 +0300

    stack

---
+++ Week 7/stack.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+public class stack {
+	public static void main(String[] args) {
+		
+	String data[] = {"java","php","python","C","c++"};
+	
+	Stack<String> s=new Stack<String>();
+	for(String word:data) {
+		s.push(word);
+	}
+	System.out.println("stack = "+s);
+	System.out.println(s.size());
+	System.out.println(s.peek());
+	s.pop();
+	System.out.println("Stack = "+s);
+	System.out.println(s.size());
+	System.out.println(s.peek());
+		
+		
+	}		
+	}
+
